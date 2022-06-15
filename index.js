48fa9fb46082b5bc7a8a05e29d4490542c58df1a
ketah <onyangoketta@gmail.com>
2022-06-16 00:47:44 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const submitData = {
+    userName: "x",
+    userEmail: "x",
+  };
+  
+
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object).newID()
+    })
+    .catch(function (error) {
+      alert("error due to");
+      console.log(error.message);
+    });
