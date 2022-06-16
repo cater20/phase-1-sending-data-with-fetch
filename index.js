d01b7518aa4fc711ac840e3cc1543590bd77ae7c
ketah <onyangoketta@gmail.com>
2022-06-16 14:50:09 +0300

    add

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 // Add your code here
-const submitData = {
-    userName: "x",
-    userEmail: "x",
-  };
-  
+const formData ={
+  name:"Steve",
+email:"steve@steve.com",
 
+
+  };
+
+  const submitData=(name,email)=>{
   const configurationObject = {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(submitData),
+      Accept:"application/json"
+  
+  },
+    body: JSON.stringify(formData),
   };
   
   fetch("http://localhost:3000/users", configurationObject)
     .then(function (response) {
       return response.json();
     })
-    .then(function (object) {
-      console.log(object).newID()
-    })
+    .then((data)=>{
+      console.log(data)
+      document.body.innerHTML=data['id']
+    
+    }
+    
+    )
     .catch(function (error) {
-      alert("error due to");
-      console.log(error.message);
+    alert ("Unauthorized Access");
+     console.log("Unauthorized Access")
+      Document.body.innerHTML='Unauthorized Access'
     });
+  }
